22f24e88c6097840b9296f526d027081fce1e1c5
Tyler Caprioli <capriolityler@gmail.com>
2023-08-06 11:25:00 -0400

    InitFireBase: Create & implement auth context

---
+++ firebase/AuthContext.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { onAuthStateChanged, getAuth, User } from "firebase/auth";
+import firebase_app from "./clientApp";
+
+const auth = getAuth(firebase_app);
+type UserContext = { user: User | null };
+export const AuthContext = React.createContext<UserContext>({ user: null });
+
+export const useAuthContext = () => React.useContext<UserContext>(AuthContext);
+
+export const AuthContextProvider = (props: { children: React.ReactNode }) => {
+  const [user, setUser] = React.useState<User | null>(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {loading ? <div>Loading...</div> : props.children}
+    </AuthContext.Provider>
+  );
+};
